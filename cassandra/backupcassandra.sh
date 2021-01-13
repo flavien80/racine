52dbabf27539a89a93d6867745fe9d3be4a40d72
flavien80 <72130409+flavien80@users.noreply.github.com>
2021-01-13 16:42:54 +0100

    Update backupcassandra.sh

--- cassandra/backupcassandra.sh
+++ cassandra/backupcassandra.sh
@@ -1,1 +1,7 @@
-nodetool snapshot 
+cd /backups/
+mkdir cassandra
+cqlsh localhost
+nodetool snapshot
+for dossier in 'find -name snapshots'
+  cp -r dossier /backups/cassandra
+restic -r /backups/cassandra --verbose backup ~/work
