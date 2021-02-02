d78edd775cd9b14c8b031c60a80df95287731290
flavien80 <72130409+flavien80@users.noreply.github.com>
2021-02-02 15:38:07 +0100

    Add files via upload

--- Tests 20 janvier/ipfsbackup.sh
+++ Tests 20 janvier/ipfsbackup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "IPFS Backup in progress"
 cd /backups
-python3 ipfsbackup.py
+python3 /ipfsbackup.py
 restic -r /backups/ --verbose backup ~/work
--- Tests 20 janvier/ipfsbackuperror.sh
+++ Tests 20 janvier/ipfsbackuperror.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 echo "Error during IPFS Backup"
